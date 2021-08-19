381df6abcaf0fcfcd8387f1d05149a3fd28de3aa
scampos321 <sebastian.campos321@gmail.com>
2021-08-19 13:09:28 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([{
+        const questions = [];
+    }])
+
+
+
+
+
+
+
+
+
+
+function writeToFile(fileName, data) {}
