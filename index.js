fb2c643afd665bae5fb58b4d30da46fd8e485c9a
scampos321 <sebastian.campos321@gmail.com>
2021-08-19 13:34:21 -0400

    working terminal commands

--- index.js
+++ index.js
@@ -2,8 +2,39 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 inquirer
     .prompt([{
-        const questions = [];
-    }])
+            type: 'input',
+            message: 'What is your project title?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Write a short description',
+            name: 'desc',
+
+        },
+        {
+            type: 'checkbox',
+            message: 'What license do you prefer?',
+            name: 'license',
+            choices: ['BSD', 'MIT', 'GPL'],
+            default: 'MIT'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username? ',
+            name: 'gh',
+        },
+        {
+            type: 'input',
+            message: 'What is your email? ',
+            name: 'email',
+        },
+    ])
+    .then(answers => {
+        console.log('Success!', answers.name);
+    });
+
+
 
 
 
