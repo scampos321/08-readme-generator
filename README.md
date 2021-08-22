5a7f49f51a88791aafa7b157cdf7aaf004c42f65
scampos321 <sebastian.campos321@gmail.com>
2021-08-21 22:03:41 -0400

    working readme license WIP

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 08-readme-generator
+
+    ## Description
+    This is a readme generator
+    ## Table of Contents
+
+    ## Installation
+    npm i
+    ## Usage
+    run this app using npm i
+    ## Contributing
+    git push
+    ## Tests
+    npm test
+    ## GitHub User and Email
+    sebcampos321
+    seb.campos03@gmail.com
--- index.js
+++ index.js
@@ -1,48 +1,79 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-inquirer
-    .prompt([{
-            type: 'input',
-            message: 'What is your project title?',
-            name: 'name',
-        },
-        {
-            type: 'input',
-            message: 'Write a short description',
-            name: 'desc',
+const { isRegExp } = require('util');
+const {
+    generateMarkdown
+} = require('./utils/generateMarkdown')
+const questions = [{
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
 
-        },
-        {
-            type: 'checkbox',
-            message: 'What license do you prefer?',
-            name: 'license',
-            choices: ['BSD', 'MIT', 'GPL'],
-            default: 'MIT'
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub username? ',
-            name: 'gh',
-        },
-        {
-            type: 'input',
-            message: 'What is your email? ',
-            name: 'email',
-        },
-    ])
-    .then(answers => {
-        console.log('Success!', answers.name);
-    });
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project',
+        name: 'desc',
 
+    },
+    {
+        type: 'list',
+        message: 'What license do you prefer?',
+        name: 'license',
+        choices: ['BSD', 'MIT', 'GPL'],
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'depen',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using your repo?',
+        name: 'using',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contri',
+    },
+];
 
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+                // console.log('ANSWERS=>', answers)
+                    //I want to take the answers an feed them into the generateMArkdown function
+                    //from that you get back a string to be written to a md file
 
+                var content = generateMarkdown(answers);
+                // console.log('CONTENT===>', content)
+                fs.writeFile('README.md', content, err => {
+                    if(err) {
+                        console.log(err, "this is an error")
+                        return;
+                    } else {
+                        console.log ('\x1b[35m%s\x1b[0m', "Generating README...");
+                    }
+                })
+                
 
+            })
+                    
+}
+    
 
-
-
-
-
-
-
-
-function writeToFile(fileName, data) {}
+    init();
